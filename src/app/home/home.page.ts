c29fd4324d13c5d800f4ffbb052d1c1719313e15
Mayara Quadros <g00472144@atu.ie>
2026-04-13 16:02:23 +0100

    adding favoutite games to an array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,29 +9,29 @@ import { addIcons } from 'ionicons';
 import { chevronBackOutline, chevronForwardOutline, heart, pricetagOutline } from 'ionicons/icons';
 
 
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [ IonCardContent, IonIcon, IonSearchbar, IonButton, IonButtons, RouterLink, IonCol, IonGrid, IonRow, IonCard, IonCardHeader, IonCardTitle, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCardContent, IonIcon, IonSearchbar, IonButton, IonButtons, RouterLink, IonCol, IonGrid, IonRow, IonCard, IonCardHeader, IonCardTitle, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   @ViewChild(IonContent) content!: IonContent;
   games: any[] = [];
   searchWord: string = "";
+  favouriteArray: any[] = [];
 
 
-  constructor(private gameService: GameService) { 
-    addIcons({heart, chevronBackOutline, chevronForwardOutline, pricetagOutline})
+  constructor(private gameService: GameService) {
+    addIcons({ heart, chevronBackOutline, chevronForwardOutline, pricetagOutline })
   }
 
   ionViewWillEnter() {
-    if(this.searchWord == "")
+    if (this.searchWord == "")
       this.loadGames();
     else
       this.searchGames(this.searchWord);
-    
+
   }
 
   loadGames() {
@@ -49,8 +49,8 @@ export class HomePage {
     this.gameService.prevPage();
     this.loadGames();
     this.content.scrollToTop(500);
-    
-    
+
+
   }
 
   onNextButton() {
@@ -70,4 +70,21 @@ export class HomePage {
       );
     }
   }
+
+  favouriteClicked(event: Event, game: any) {
+    if (game.isFav) {
+      game.isFav = false;
+      let index = this.favouriteArray.indexOf(game);
+      this.favouriteArray.splice(index, 1);
+    }
+    else {
+      game.isFav = true;
+      this.favouriteArray.push(game);
+    }
+    console.log(this.favouriteArray);
+  }
+  
+
 }
+
+
