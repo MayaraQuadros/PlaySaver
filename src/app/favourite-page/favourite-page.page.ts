62225c089215ee51e26ed02a6971ed1fae19793d
Mayara Quadros <g00472144@atu.ie>
2026-04-22 21:59:31 +0100

    platforms icons in favourite page added

--- src/app/favourite-page/favourite-page.page.ts
+++ src/app/favourite-page/favourite-page.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem } from '@ionic/angular/standalone';
 import { RouterLink, } from '@angular/router';
 import { IonCol, IonGrid, IonRow } from '@ionic/angular/standalone';
 import { IonIcon, IonButton, IonButtons, IonSearchbar } from '@ionic/angular/standalone';
-import { IonCardContent, IonCard, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
 import { GameService } from '../services/game-service';
 import { addIcons } from 'ionicons';
-import { home, chevronBackOutline, chevronForwardOutline, heart, pricetagOutline } from 'ionicons/icons';
+import { desktopOutline ,logoXbox, logoPlaystation, home, chevronBackOutline, chevronForwardOutline, heart, pricetagOutline } from 'ionicons/icons';
 
 
 
@@ -20,15 +20,15 @@ import { home, chevronBackOutline, chevronForwardOutline, heart, pricetagOutline
   templateUrl: './favourite-page.page.html',
   styleUrls: ['./favourite-page.page.scss'],
   standalone: true,
-  imports: [IonCard, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonRow, IonIcon, RouterLink, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonRow, IonIcon, RouterLink, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem]
 })
 export class FavouritePagePage implements OnInit {
 
   constructor(public gameService: GameService) {
-    addIcons({ home, heart, chevronBackOutline, chevronForwardOutline, pricetagOutline })
+    addIcons({ desktopOutline ,logoXbox, logoPlaystation, home, heart, chevronBackOutline, chevronForwardOutline, pricetagOutline })
   }
-  
-  ngOnInit() {}
+
+  ngOnInit() { }
 
   async ionViewWillEnter() {
     await this.gameService.getFavourites();
@@ -37,13 +37,30 @@ export class FavouritePagePage implements OnInit {
   favouriteClicked(event: Event, game: any) {
     let index = -1;
     game.isFav = false;
-    for(let i = 0; i < this.gameService.favouriteArray.length; i++)
-    {
-      if(this.gameService.favouriteArray[i].id == game.id)
+    for (let i = 0; i < this.gameService.favouriteArray.length; i++) {
+      if (this.gameService.favouriteArray[i].id == game.id)
         index = i;
     }
     this.gameService.favouriteArray.splice(index, 1);
     this.gameService.saveFavourite();
 
+  }
+
+  findPlatform() {
+    if (this.gameService.favouriteArray != null) {
+
+      for (let i = 0; i < this.gameService.favouriteArray.length; i++) {
+
+        if (this.gameService.favouriteArray[i].parent_platforms?.some((plat: any) => plat.platform.id === 1)) {
+          this.gameService.favouriteArray[i].pc = true;
+        }
+        if (this.gameService.favouriteArray[i].parent_platforms?.some((plat: any) => plat.platform.id === 2)) {
+          this.gameService.favouriteArray[i].playStation = true;
+        }
+        if (this.gameService.favouriteArray[i].parent_platforms?.some((plat: any) => plat.platform.id === 3)) {
+          this.gameService.favouriteArray[i].xbox = true;
+        }
+      }
+    }
   }
 }
